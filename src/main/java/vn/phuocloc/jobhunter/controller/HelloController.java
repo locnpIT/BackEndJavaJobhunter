8876f79c8dafea3ec43dc8c6de6508c4c62f23e2
locnpIT <npl2004qn@gmail.com>
2024-07-03 18:30:01 +0700

    upload code

--- src/main/java/vn/phuocloc/jobhunter/controller/HelloController.java
+++ src/main/java/vn/phuocloc/jobhunter/controller/HelloController.java
@@ -1,4 +1,4 @@
-package vn.hoidanit.jobhunter.controller;
+package vn.phuocloc.jobhunter.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String getHelloWorld() {
-        return "Hello World (Hỏi Dân IT & Eric)";
+        return "Xin chao nguyen phuoc loc";
     }
 }
