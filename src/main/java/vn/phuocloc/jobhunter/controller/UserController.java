9f1ca94b6cbde5250acbb3e7808e8b8b426eaba7
locnpIT <npl2004qn@gmail.com>
2024-07-06 14:45:42 +0700

    upload code

--- src/main/java/vn/phuocloc/jobhunter/controller/UserController.java
+++ src/main/java/vn/phuocloc/jobhunter/controller/UserController.java
@@ -1,10 +1,19 @@
 package vn.phuocloc.jobhunter.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import vn.phuocloc.jobhunter.domain.User;
 import vn.phuocloc.jobhunter.service.UserService;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class UserController {
@@ -15,15 +24,41 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/user/create")
-    public String createNewUser() {
+    // @GetMapping("/user/create")
+    @PostMapping("/user")
+    public User createNewUser(@RequestBody User postmanUser) {
 
         User user = new User();
         user.setEmail("npl2004qn@gmail.com");
         user.setName("Phuoc Loc");
         user.setPassword("123456");
-        this.userService.handleCreateUser(user);
-        return "create user";
+        User userCreate = this.userService.handleCreateUser(postmanUser);
+        return userCreate;
+    }
+
+    @DeleteMapping("/user/{id}")
+    public String deleteUser(@PathVariable("id") long id) {
+        this.userService.deleteUserById(id);
+        return "phuoclocne";
+    }
+
+    @GetMapping("/user/{id}")
+    public User fetchUserById(@PathVariable("id") long id) {
+        Optional<User> userGet = this.userService.fetchById(id);
+        if (userGet.isPresent()) {
+            return userGet.get();
+        }
+        return null;
+    }
+
+    @GetMapping("/user")
+    public List<User> findAllUser() {
+        return this.userService.findAllUsers();
+    }
+
+    @PutMapping("/user")
+    public User updateUser(@RequestBody User userCreate) {
+        return this.userService.handleUpdateUser(userCreate);
     }
 
 }
--- src/main/java/vn/phuocloc/jobhunter/service/UserService.java
+++ src/main/java/vn/phuocloc/jobhunter/service/UserService.java
@@ -1,5 +1,8 @@
 package vn.phuocloc.jobhunter.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import vn.phuocloc.jobhunter.domain.User;
@@ -16,6 +19,29 @@ public class UserService {
 
     public User handleCreateUser(User user) {
         return this.userRepository.save(user);
+    }
+
+    public void deleteUserById(long id) {
+        this.userRepository.deleteById(id);
+    }
+
+    public Optional<User> fetchById(long id) {
+        return userRepository.findById(id);
+    }
+
+    public List<User> findAllUsers() {
+        return this.userRepository.findAll();
+    }
+
+    public User handleUpdateUser(User userCreate) {
+        User userUpdate = this.fetchById(userCreate.getId()).get();
+        if (userUpdate != null) {
+            userUpdate.setEmail(userCreate.getEmail());
+            userUpdate.setPassword(userCreate.getPassword());
+            userUpdate.setName(userCreate.getName());
+            this.handleCreateUser(userUpdate);
+        }
+        return userUpdate;
     }
 
 }
